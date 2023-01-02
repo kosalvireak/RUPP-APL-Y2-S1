89247535398a93a3de198b04769ef1090bc608ba
Kosalvireak Khoeun <virakvary@gmail.com>
2023-01-02 20:48:03 +0700

    add loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        float number = 65f;
         System.out.println("Hello world!");
+        for (int hey = 0 ; hey <= number ; hey ++)
+        {
+            System.out.println(hey*2);
+        }
     }
 }
