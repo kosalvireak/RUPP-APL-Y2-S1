742bb8e0339d510b8129f3678872d1cbdbb45ed4
Kosalvireak Khoeun <virakvary@gmail.com>
2023-01-18 21:37:30 +0700

    modify dog class

--- Homework/src/HarmonicSum.java
+++ Homework/src/HarmonicSum.java
@@ -1,18 +1,20 @@
 public class HarmonicSum {
     public static void main(String[] args)
     {
-        float sum_l2r = 0f, sum_r2l=0f;
-        int n = 50000, j = 1;
+        double sum_l2r = 0f, sum_r2l=0f;
+        int n = 50000;
         for (int i = 1 ; i <= n; i ++){
             sum_l2r = sum_l2r + 1f/i;
+
         }
         System.out.println("harmonic left to right: "+sum_l2r);
 
-        for (int m = n ; m >= j; m --){
-            sum_r2l = sum_r2l + 1f/m;
-//            System.out.println("harmonic right to left: "+sum_r2l);
-
+        for (int j = n ; j >= 1; j --){
+            sum_r2l = sum_r2l + 1f/j;
         }
         System.out.println("harmonic right to left: "+sum_r2l);
+        double absDiff = Math.abs(sum_l2r - sum_r2l);
+        System.out.printf("The absolute difference  is: %.30f",absDiff);
+//        Both of the value are the same, but if I use float as its data type the value with change due to round.
     }
 }
---
+++ src/mainclass/Dog.java
@@ -0,0 +1,16 @@
+package mainclass;
+
+public class Dog {
+    //Field
+    String Name, Bread;
+    float size;
+    int age;
+    //Method
+    public void Run(){
+        System.out.print("This is the Run method in Dog class ");
+    }
+    void Eat(){
+        System.out.print("This is the Eat method in Dog class ");
+
+    }
+}
